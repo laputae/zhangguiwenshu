06d60cc40850fb5800a1d0403454432fd743bf2e
laputae <ahahahay51@gmail.com>
2026-05-21 11:54:46 +0800

    增加全文索引功能

--- app/repositories/qdrant/column_qdrant_repository.py
+++ app/repositories/qdrant/column_qdrant_repository.py
@@ -1,4 +1,5 @@
 from qdrant_client import AsyncQdrantClient
+from qdrant_client.http.models import PointStruct
 from qdrant_client.models import VectorParams, Distance
 
 from app.conf.app_config import app_config
@@ -16,3 +17,9 @@ class ColumnQdrantRepository:
                 collection_name=self.collection_name,
                 vectors_config=VectorParams(size=app_config.qdrant.embedding_size, distance=Distance.COSINE)
             )
+
+    async def upsert(self, ids: list[str], embeddings: list[list[float]], payloads: list[dict], batch_size: int = 10):
+        points: list[PointStruct] = [PointStruct(id=id, vector=embedding, payload=payload) for id, embedding, payload in
+                                     zip(ids, embeddings, payloads)]
+        for i in range(0, len(points), batch_size):
+            await self.client.upsert(collection_name=self.collection_name, points=points[i:i + batch_size])
--- app/services/meta_knowledge_service.py
+++ app/services/meta_knowledge_service.py
@@ -8,6 +8,8 @@ from omegaconf import OmegaConf
 from app.conf.meta_config import MetaConfig
 from app.entities.column_info import ColumnInfo
 from app.entities.table_info import TableInfo
+from app.entities.value_info import ValueInfo
+from app.repositories.es.value_es_repository import ValueESRepository
 from app.repositories.mysql.dw.dw_mysql_repository import DWMySQLRepository
 from app.repositories.mysql.meta.meta_mysql_repository import MetaMySQLRepository
 from app.repositories.qdrant.column_qdrant_repository import ColumnQdrantRepository
@@ -17,11 +19,13 @@ class MetaKnowledgeService:
     def __init__(self, meta_mysql_repository: MetaMySQLRepository,
                  dw_mysql_repository: DWMySQLRepository,
                  column_qdrant_repository: ColumnQdrantRepository,
-                 embedding_client: HuggingFaceEndpointEmbeddings):
+                 embedding_client: HuggingFaceEndpointEmbeddings,
+                 value_es_repository: ValueESRepository):
         self.meta_mysql_repository = meta_mysql_repository
         self.dw_mysql_repository = dw_mysql_repository
         self.column_qdrant_repository = column_qdrant_repository
         self.embedding_client: HuggingFaceEndpointEmbeddings = embedding_client
+        self.value_es_repository: ValueESRepository = value_es_repository
 
     async def build(self, config_path: Path):
         context = OmegaConf.load(config_path)
@@ -79,6 +83,23 @@ class MetaKnowledgeService:
                 batch_embedding_texts = embedding_texts[i:i + embedding_batch_size]
                 batch_embeddings = await self.embedding_client.aembed_documents(batch_embedding_texts)
                 embeddings.extend(batch_embeddings)
-
+            ids = [point['id'] for point in points]
+            payloads = [point['payload'] for point in points]
+            await self.column_qdrant_repository.upsert(ids, embeddings, payloads)
+            # 对指定字段取值做全文索引
+            await self.value_es_repository.ensure_index()
+            value_infos: list[ValueInfo] = []
+            for table in meta_config.tables:
+                for column in table.columns:
+                    if column.sync:
+                        current_column_values = await self.dw_mysql_repository.get_column_values(table.name,
+                                                                                                 column.name, 100000)
+                        current_values_infos = [ValueInfo(id=f"{table.name}.{column.name}.{current_column_value}",
+                                                          value=current_column_value,
+                                                          column_id=f"{table.name}.{column.name}") for
+                                                current_column_value in
+                                                current_column_values]
+                        value_infos.extend(current_values_infos)
+            await self.value_es_repository.index(value_infos)
         if meta_config.metrics:
             pass

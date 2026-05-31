eb40614f65404a2321f8d843adfdad4c9fafe473
laputae <ahahahay51@gmail.com>
2026-05-31 18:12:22 +0800

    增加功能：在过滤指标信息里打印日志

--- app/agent/nodes/filter_metric.py
+++ app/agent/nodes/filter_metric.py
@@ -2,7 +2,7 @@ import yaml
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
-
+from app.core.log import logger
 from app.agent.context import DataAgentContext
 from app.agent.llm import llm
 from app.agent.state import DataAgentState, MetricInfoState
@@ -20,4 +20,5 @@ async def filter_metric(state: DataAgentState, runtime: Runtime[DataAgentContext
     result = await chain.ainvoke({"query": query,
                                   "metric_infos": yaml.dump(metric_infos, allow_unicode=True, sort_keys=False)})
     metric_infos = [metric_info for metric_info in metric_infos if metric_info["name"] in result]
+    logger.info(f"过滤后的指标信息是: {metric_infos}")
     return {"metric_infos": metric_infos}

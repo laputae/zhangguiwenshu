7fb163d40bca966b981b51a35427ecba6e8d07d4
laputae <ahahahay51@gmail.com>
2026-05-31 17:10:11 +0800

    增加功能：过滤表信息

--- app/agent/nodes/filter_table.py
+++ app/agent/nodes/filter_table.py
@@ -1,20 +1,24 @@
 import asyncio
 
+import yaml
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent import llm
 from app.agent.context import DataAgentContext
-from app.agent.state import DataAgentState
+from app.agent.state import DataAgentState, TableInfoState
 from app.prompt.prompt_loader import load_prompt
 
 
-async def filter_table(state:DataAgentState,runtime:Runtime[DataAgentContext]):
+async def filter_table(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     writer = runtime.stream_writer
     writer("过滤表格信息")
-
-    prompt = PromptTemplate(template=load_prompt("filter_table_info"), input_variables=[])
+    query = state["query"]
+    table_infos: list[TableInfoState] = state["table_infos"]
+    prompt = PromptTemplate(template=load_prompt("filter_table_info"), input_variables=["query", "table_infos"])
     output_parser = JsonOutputParser()
     chain = prompt | llm | output_parser
+    await chain.ainvoke({"query": query,
+                         "table_infos": yaml.dump(table_infos, allow_unicode=True, sort_keys=False)})
     

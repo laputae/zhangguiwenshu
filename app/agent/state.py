cad98578548e7fdbcfd9b3e56787ebaf7f026e2f
laputae <ahahahay51@gmail.com>
2026-06-01 09:13:18 +0800

    状态信息中增加date_info和db_info

--- app/agent/state.py
+++ app/agent/state.py
@@ -4,25 +4,40 @@ from app.entities.column_info import ColumnInfo
 from app.entities.metric_info import MetricInfo
 from app.entities.value_info import ValueInfo
 
+
 class MetricInfoState(TypedDict):
     name: str
-    description:str
-    relevant_columns:list[str]
-    alias:list[str]
+    description: str
+    relevant_columns: list[str]
+    alias: list[str]
+
 
 class ColumnInfoState(TypedDict):
     name: str
     type: str
-    role:str
-    examples:list
-    description:str
-    alias:list[str]
+    role: str
+    examples: list
+    description: str
+    alias: list[str]
+
 
 class TableInfoState(TypedDict):
-    name:str
-    role:str
-    description:str
-    columns:list[ColumnInfoState]
+    name: str
+    role: str
+    description: str
+    columns: list[ColumnInfoState]
+
+
+class DateInfoState(TypedDict):
+    date: str
+    weekday: str
+    quarter: str
+
+
+class DBInfoState(TypedDict):
+    dialect: str
+    version: str
+
 
 class DataAgentState(TypedDict):
     # 用户输入的查询
@@ -37,5 +52,9 @@ class DataAgentState(TypedDict):
     retrieved_metric_infos: list[MetricInfo]
     # 检索到的字段取值信息
     retrieved_value_infos: list[ValueInfo]
+
     table_infos: list[TableInfoState]
     metric_infos: list[MetricInfoState]
+
+    date_info: DateInfoState
+    db_info: DBInfoState

a473d68b46ddc1f0ee3b22b59c88ca48fc830b8f
laputae <ahahahay51@gmail.com>
2026-06-02 11:11:21 +0800

    开始编写web服务

---
+++ app/services/query_service.py
@@ -0,0 +1,6 @@
+class QueryService:
+    def __init__(self):
+        pass
+
+    async def query(self, query: str):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+from app.api.routers.query_router import query_router
+
+app = FastAPI()
+app.include_router(query_router)

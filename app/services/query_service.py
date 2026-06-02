5400e2f872a635d3ce4da5761e672676ff04cd49
laputae <ahahahay51@gmail.com>
2026-06-02 16:27:07 +0800

    修复bug:query_service

--- app/services/query_service.py
+++ app/services/query_service.py
@@ -6,6 +6,8 @@ from app.agent.state import DataAgentState
 from app.repositories.es.value_es_repository import ValueESRepository
 from app.repositories.mysql.dw.dw_mysql_repository import DWMySQLRepository
 from app.repositories.mysql.meta.meta_mysql_repository import MetaMySQLRepository
+from app.repositories.qdrant.column_qdrant_repository import ColumnQdrantRepository
+from app.repositories.qdrant.metric_qdrant_repository import MetricQdrantRepository
 
 
 class QueryService:

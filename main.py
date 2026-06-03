cebf8c7465e73abab302adebe8a2f3ed245c2a69
laputae <ahahahay51@gmail.com>
2026-06-03 17:07:40 +0800

    完善日志框架，根据id区分不同日志

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+import uuid
+
+from fastapi import FastAPI, Request
 
 from app.api.lifespan import lifespan
 from app.api.routers.query_router import query_router
+from app.core.context import request_id_ctx_var
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(query_router)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    # 请求被处理
+    request_id = uuid.uuid4()
+    request_id_ctx_var.set(request_id)
+    response = await call_next(request)
+    # 请求被处理之后
+    return response
